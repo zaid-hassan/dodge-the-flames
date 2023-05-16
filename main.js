152b6bded395df66999386b73b7d630f85a4d733
zaid-hassan <zaidhassan251@gmail.com>
2023-05-16 18:31:09 +0530

    workig on game over

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function jump(){
 let checkgame = setInterval(function(){
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130){
+    if (blockLeft < 120 && blockLeft > && characterTop >= 130){
         block.style.animation = "none";
         block.style.display = "none";
         alert("You lose!");
