a4c172d0b3db083ec7b041038751a0f73438281b
zaid-hassan <zaidhassan251@gmail.com>
2023-05-16 18:07:18 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+let character = document.getElementById("character");
+let block = document.getElementById("block");
+function jump(){
+    if (character.classList != "animation"){
+        character.classList.add("animation");
+    }
+    setTimeout(function(){
+        character.classList.remove("animation");
+    }, 500)
+}
+
+let checkgame = setInterval(function(){
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130){
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("You lose!");
+    }
+},10)
